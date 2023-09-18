a1c0e3f26a523b4767951cfc880b324cda557374
johan_helm <jou@ngs.ru>
2023-09-19 05:56:54 +0700

    work on install.sh

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 This application is command line interface youtube client. It allows to watch videos from youtube without google advertisements and advertising integrations.
+
+INSTALLATION
+1. paste install.sh at your device home directory
+2. make it executable by chmod 744 install.sh
+3. execute it
+
+USAGE
+
+ctrl+f      resolution change
--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt update
-apt install wget mpv ffmpeg yt-dlp git
+apt install -y wget mpv ffmpeg yt-dlp git
 ### Cкрипт установки и запуска приложения
 # Укажите в какую директорию вы хотите установить
 your_dir=~/youtube-cli
@@ -9,18 +9,39 @@ your_dir=~/youtube-cli
 mkdir $your_dir
 cd $your_dir
 # Установить венв
-python3 -m venv $your_dir
+apt install -y python3-venv
+python3 -m venv venv
 # Запустить венв
-source $your_dir/bin/activate
+source $your_dir/venv/bin/activate
 # Установить pip
 wget https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py
 rm get-pip.py
 
-git clone  
-pip install -R requirements.txt
+git clone https://github.com/JohanHelm/cli_youtube.git
+
+pip install -r ./requirements.txt
 #  Установка самого свежего mpv из деб пакета
 # wget https://deb-multimedia.org/pool/main/m/mpv-dmo/mpv_0.36.0-dmo2_amd64.deb
 # отсюда https://deb-multimedia.org/dists/testing/main/binary-amd64/package/mpv
 # или отсюда https://fruit.je/apt
 
+# create alias for app
+echo "alias youtube='~/youtube-cli/venv/bin/python main.py'">> ~/.bashrc
+source ~/.bashrc
+
+
+# paste scripts into ~/.config/mpv/scripts
+# sponsoreblock
+# https://github.com/po5/mpv_sponsorblock
+wget https://github.com/po5/mpv_sponsorblock/raw/master/sponsorblock.lua
+mkdir sponsorblock_shared
+cd sponsorblock_shared
+wget https://github.com/po5/mpv_sponsorblock/raw/master/sponsorblock_shared/main.lua
+wget https://github.com/po5/mpv_sponsorblock/raw/master/sponsorblock_shared/sponsorblock.py
+
+# resolution
+# https://github.com/jgreco/mpv-youtube-quality
+wget https://github.com/jgreco/mpv-youtube-quality/raw/master/youtube-quality.lua
+
+    
