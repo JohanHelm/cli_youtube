28c8708d886ea76ce160d4e4880c786ff8490c70
johan_helm <jou@ngs.ru>
2023-10-06 14:17:50 +0700

    update button

--- menu/menus.py
+++ menu/menus.py
@@ -53,7 +53,7 @@ class MenuGenerator:
             cmd = f'git pull origin cli'
             update_request = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True, universal_newlines=True)
             update_result = update_request.communicate()[0]
-            if update_result != 'Already up to date.':
+            if 'Already up to date.' not in update_result:
                 quit(0)
         elif selected_item == len(menu_items) - 1:  # 'Exit.'
             quit(0)
